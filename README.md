bb958462a40fbc47509385ec8b1939cff55df051
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-03-02 17:48:36 +0100

    🎉 Beginnings of MMM-NiceHash

---
+++ README.md
@@ -0,0 +1,31 @@
+# MMM-NiceHash
+A module for Michael Teeuw's MagicMirror project that displays your NiceHash statistics. 
+**This module is currently in development; Actually, there's nothing related to MagicMirror. These are only tests**
+
+## Installing
+Installing the module is quite straight forward
+### Step 1 - Download the module
+```bash
+git clone https://github.com/mlbonniec/MMM-NiceHash.git
+cd MMM-NiceHash
+```
+
+### Step 2 - Add your credentials to a `.env` file
+Copy the `.env.sample` file to `.env`, using this bash command, or simply copy / paste the file.
+```bash
+cp .env.sample .env
+```
+Once you've created the file, open it and fill it with your credentials.
+
+### Step 3 - Install and run
+Install npm dependencies
+```bash
+npm install
+npm start
+```
+
+<!-- ## Use -->
+<!-- Now you can fully use MagicMirror and the MMM-NiceHash module to display your statistics ! Enjoy it well ! -->
+
+## Contributing
+If you find any problems, bugs or have questions, please [open a GitHub issue](https://github.com/mlbonniec/MMM-NiceHash/issues) in this repository.
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError, Method } from 'axios';
+import { v4 as uuid } from 'uuid';
+import { createHmac } from 'crypto';
+
+const { API_KEY, API_SECRET_KEY, ORGANIZATION_ID } = process.env;
+
+const nonce: string = uuid();
+const requestId: string = uuid();
+const endpoint: string = '/main/api/v2/mining/rigs/stats/unpaid';
+
+function hmacSHA256(apiKey: string, time: number, nonce: string, organizationId: string, method: Method, endpoint: string, query?: string): string {
+	const hmac = createHmac('sha256', API_SECRET_KEY);
+	const data = `${apiKey}\0${time}\0${nonce}\0\0${organizationId}\0\0${method}\0${endpoint}\0${query ?? ''}`;
+
+	return hmac.update(data).digest('hex');
+}
+
+(async function request(): Promise<void> {
+	const { data: { serverTime: timestamp } }:  { data: { serverTime: number } } = await axios.get('https://api2.nicehash.com/api/v2/time');
+	const hmac = hmacSHA256(API_KEY, timestamp, nonce, ORGANIZATION_ID, 'GET', endpoint, null);
+
+	axios({
+		url: 'https://api2.nicehash.com' + endpoint,
+		method: 'GET',
+		headers: {
+			'X-Time': timestamp,
+			'X-Nonce': nonce,
+			'X-Organization-Id': ORGANIZATION_ID,
+			'X-Request-Id': requestId,
+			'X-Auth': `${API_KEY}:${hmac}`
+		},
+	})
+	.then(res => console.log(res.data))
+	.catch((error: AxiosError) => console.log(error.response.data));
+})();
