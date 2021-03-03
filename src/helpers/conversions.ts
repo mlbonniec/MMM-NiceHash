3bd00e8093fc3562c05337a2e751cca12bfb0b1e
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-03-03 23:38:16 +0100

    ✨ Introduce conversion helper

---
+++ src/helpers/conversions.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const currencies = ['USD', 'AUD', 'BRL', 'CAD', 'CHF', 'CLP', 'CNY', 'DKK', 'EUR', 'GBP', 'HKD', 'INR', 'ISK', 'JPY', 'KRW', 'NZD', 'PLN', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'TWD'] as const;
+
+interface Price {
+  '15m': number;
+  last: number;
+  buy: number;
+  sell: number;
+  symbol: string;
+}
+
+type Currencies = typeof currencies[number];
+type Prices = Record<Currencies, Price>;
+
+export async function toCurrency (value: number, currency: Currencies): Promise<number> {
+	if (!currencies.includes(currency))
+		throw new Error(`${currency} is not a valid currency.`);
+		
+	const { data }: { data: Prices } = await axios.get('https://blockchain.info/ticker');
+		
+	return data[currency].sell * value;
+}
+
+export function perWeek(value: number): number {
+	return 7 * value;
+}
+
+export function perMonth(value: number): number {
+	const d = new Date();
+	
+	return new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate() * value;
+}
+
+export function perYear(value: number): number {
+	const d = new Date();
+	let days: number = 0;
+	
+	for (let i = 1; i <= 12; i++) {
+		days += new Date(d.getFullYear(), i + 1, 0).getDate();
+	}
+	
+	return days * value;
+}
