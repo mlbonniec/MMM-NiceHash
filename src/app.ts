8612f48906629674115984c3fc6433bcae204951
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-03-03 23:39:05 +0100

    ✨ Use of new helpers, and display rigs stats

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,30 @@
-import axios, { AxiosError, Method } from 'axios';
-import { v4 as uuid } from 'uuid';
-import { createHmac } from 'crypto';
+import NiceHash from './helpers/nicehash';
+import { perWeek, perMonth, perYear, toCurrency } from './helpers/conversions';
 
 const { API_KEY, API_SECRET_KEY, ORGANIZATION_ID } = process.env;
-
-const nonce: string = uuid();
-const requestId: string = uuid();
-const endpoint: string = '/main/api/v2/mining/rigs/stats/unpaid';
-
-function hmacSHA256(apiKey: string, time: number, nonce: string, organizationId: string, method: Method, endpoint: string, query?: string): string {
-  const data = `${apiKey}\0${time}\0${nonce}\0\0${organizationId}\0\0${method}\0${endpoint}\0${query ?? ''}`;
-
-  return createHmac('sha256', API_SECRET_KEY).update(data).digest('hex');
-}
+const nh = new NiceHash(API_KEY, API_SECRET_KEY, ORGANIZATION_ID);
 
 (async function request(): Promise<void> {
-  const timestamp = Date.now();
-  const hmac = hmacSHA256(API_KEY, timestamp, nonce, ORGANIZATION_ID, 'GET', endpoint, null);
-
   try {
-    /* Test data, because I don't have mining rigs */
-    // const data = [
-    //   [1602528000000, 0, 0.00000518, 7e-8, 0.000009475961146660826, 0],
-    //   [1602528000000, 3, 0.00000518, 2.2e-7, 0.000009475961146660826, 0],
-    //   [1601923500000, 53, 0.00008685, 0.00007425, 0.00004878825729289759, 0]
-    // ];
-    const { data: prices } = await axios({ url: 'https://blockchain.info/ticker' });
-    const { data: { data } } = await axios({
-      baseURL: 'https://api2.nicehash.com',
-      url: endpoint,
-      method: 'GET',
-      headers: {
-        'X-Time': timestamp,
-        'X-Nonce': nonce,
-        'X-Organization-Id': ORGANIZATION_ID,
-        'X-Request-Id': requestId,
-        'X-Auth': `${API_KEY}:${hmac}`
-      },
+    const { data } = await nh.getRigs();
+    const { totalProfitability, miningRigs }: { totalProfitability: number, miningRigs: Record<string, any>[] } = data ?? {};
+    const rigs = miningRigs.map(rig => {
+      const temperatures = rig.devices.map(devices => `name: ${devices.name}, temp: ${devices.temperature}`);
+      
+      return { name: rig.name, id: rig.rigId, status: rig.minerStatus, temperatures };
     });
 
-    const profitabilities: number[] = data.map(e => e[4]);
-    const profitability: number = profitabilities.reduce((a, b) => a + b, 0);
-
-    console.log(`You should earn ${profitability} BTC today. (but I'm not sure...)`);
-    console.log(`${profitability} BTC should represents ${profitability * prices.USD.sell} USD.`);
-    console.log(`\nIf my calculation is wrong, the next one should be better.\n`);
-
-    const profitability2 = data[0]?.[4] ?? 0;
-
-    console.log(`You should earn ${profitability2} BTC today. (but I'm not sure...)`);
-    console.log(`${profitability2} BTC should represents ${profitability2 * prices.USD.sell} USD.`);
+    console.log(rigs);
+    console.log();
+    
+    console.log('Your total earnings:')
+    console.log('Per day in BTC:', totalProfitability, 'and in USD:', await toCurrency(totalProfitability, 'USD'));
+    console.log('Per week:', perWeek(totalProfitability));
+    console.log('Per month:', perMonth(totalProfitability));
+    console.log('Per year:', perYear(totalProfitability));
   } catch (unknownError: unknown) {
-    const error = (unknownError as AxiosError);
-
+    const error = (unknownError as Error);
+    
     console.log(error.message);
   }
 })();
