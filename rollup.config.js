8ee55cbb2bde53b29f26d10d20a82b3faae0b1b2
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-03-04 22:47:11 +0100

    ➖ Remove rollup-json

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,27 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import json from '@rollup/plugin-json';
 import pkg from './package.json';
 
 export default [
-	/**
-	 * The core module file
-	 * Written in Typescript and bundled with all dependencies.
-	 */
 	{
 		input: './src/MMM-NiceHash.ts',
-		// input: './dist/MMM-NiceHash.js',
 		plugins: [
 			typescript(),
 			nodeResolve({ browser: true }),
 			commonjs(),
-			json(),
 		],
 		output: {
 			file: './MMM-NiceHash.js',
 			format: 'iife',
 		},
 	},
-	/**
-	 * The module helper file
-	 * Written in Typescript and only compiled to be used within node.
-	 */
 	{
 		input: './src/node_helper.ts',
-		// input: './dist/node_helper.js',
 		plugins: [
 			typescript(),
 			nodeResolve({ browser: true }),
 			commonjs(),
-			json(),
 		],
 		external: ['node_helper', ...Object.keys(pkg.dependencies)],
 		output: {
