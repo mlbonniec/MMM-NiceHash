22f7368abbd0a0662d6c1d7ccff31d37b8d384e4
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-03-05 18:49:05 +0100

    👷 Add dependencies to rollup However, compiled files doesn't work, they use 'undefined' dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,43 @@
 import typescript from '@rollup/plugin-typescript';
+import commonJS from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { dependencies } from './package.json';
 
 const deps = Object.keys(dependencies);
 export default [
-	{
-		input: './src/MMM-NiceHash.ts',
-		plugins: [typescript()],
-		external: [...deps],
-		output: {
-			file: './MMM-NiceHash.js',
-			format: 'umd',
-			globals: {
-				axios: 'axios',
-			},
-		},
-	},
-	{
-		input: './src/node_helper.ts',
-		plugins: [typescript()],
-		external: ['node_helper', 'crypto', ...deps],
-		output: {
-			file: './node_helper.js',
-			format: 'umd',
-			globals: {
-				axios: 'axios',
-				uuid: 'uuid',
-				crypto: 'crypto',
-			},
-		},
-	},
+  {
+    input: './src/MMM-NiceHash.ts',
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      commonJS(),
+    ],
+    external: [...deps],
+    output: {
+      file: './MMM-NiceHash.js',
+      format: 'umd',
+      globals: {
+        axios: 'axios',
+        uuid: 'uuid',
+      },
+    },
+  },
+  {
+    input: './src/node_helper.ts',
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      commonJS(),
+    ],
+    external: ['node_helper', 'crypto', ...deps],
+    output: {
+      file: './node_helper.js',
+      format: 'umd',
+      globals: {
+        axios: 'axios',
+        uuid: 'uuid',
+        crypto: 'crypto',
+      },
+    },
+  },
 ];
